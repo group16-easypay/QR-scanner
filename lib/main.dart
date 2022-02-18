686c2763fecfefe9269460189ec20831dabbba81
sophiarebecca20 <99731471+sophiarebecca20@users.noreply.github.com>
2022-02-18 14:29:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,3 @@ import 'package:sm/Intro.dart';
 
 import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.android,
-  );
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const Intro(),
-    );
-  }
-}
